290b956866054b0af02ec4af7fdc460fd40a4fbe
Caiden Block <caiden@caidenblock.com>
2025-09-02 11:33:04 -0500

    hello world

--- main.py
+++ main.py
@@ -29,25 +29,26 @@ try:
     logging.info("Drawing")    
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
+    font72 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 72)
+
     # Drawing on the Horizontal image
     logging.info("1.Drawing on the Horizontal image...") 
     HBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126
     HRYimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126  ryimage: red or yellow image  
     drawblack = ImageDraw.Draw(HBlackimage)
     drawry = ImageDraw.Draw(HRYimage)
-    drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
-    drawblack.text((10, 20), '7.5inch e-Paper bc', font = font24, fill = 0)
-    drawblack.text((150, 0), u'微雪电子', font = font24, fill = 0)    
-    drawblack.line((20, 50, 70, 100), fill = 0)
-    drawblack.line((70, 50, 20, 100), fill = 0)
-    drawblack.rectangle((20, 50, 70, 100), outline = 0)    
-    drawry.line((165, 50, 165, 100), fill = 0)
-    drawry.line((140, 75, 190, 75), fill = 0)
-    drawry.arc((140, 50, 190, 100), 0, 360, fill = 0)
-    drawry.rectangle((80, 50, 130, 100), fill = 0)
-    drawry.chord((200, 50, 250, 100), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+    drawblack.text((10, 0), 'hello world', font = font72, fill = 0)
+    # drawblack.text((10, 20), '7.5inch e-Paper bc', font = font24, fill = 0)
+    # drawblack.text((150, 0), u'微雪电子', font = font24, fill = 0)    
+    # drawblack.line((20, 50, 70, 100), fill = 0)
+    # drawblack.line((70, 50, 20, 100), fill = 0)
+    # drawblack.rectangle((20, 50, 70, 100), outline = 0)    
+    # drawry.line((165, 50, 165, 100), fill = 0)
+    # drawry.line((140, 75, 190, 75), fill = 0)
+    # drawry.arc((140, 50, 190, 100), 0, 360, fill = 0)
+    # drawry.rectangle((80, 50, 130, 100), fill = 0)
+    # drawry.chord((200, 50, 250, 100), 0, 360, fill = 0)
+    # epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
     time.sleep(2)
     
 
